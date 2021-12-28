f59ceb699359fe6ba318d5bd3338f99ce19a4f5c
Bourne <68225384@qq.com>
2021-12-28 17:59:36 +0800

    add clerk/customer;

---
+++ app/Nova/Customer.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Customer extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Customer::class;
+    public static function label()
+    {
+        return __('Customer');
+    }
+    public static function group()
+    {
+        return __("Chain Store");
+    }
+    public static function icon()
+    {
+        return view("nova::svg.".strtolower(explode('\\', self::class)[2]));
+    }
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Text::make(__('Realname'), 'name')->sortable()->rules('required', 'max:255'),
+            Text::make(__('Gender'), 'gender'),
+            Text::make(__('Mobile'), 'mobile'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+    
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        return $query->where('type', \App\Models\User::CUSTOMER);
+    }
+}
--- app/Nova/User.php
+++ app/Nova/User.php
@@ -43,7 +43,7 @@ class User extends Resource
 
     public static function label()
     {
-        return __('Customer');
+        return __('User');
     }
     public static function group()
     {
