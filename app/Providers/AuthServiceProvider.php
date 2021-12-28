a6ca3b24f1d32909ce88d5636567e51711415da7
Bourne <68225384@qq.com>
2021-12-28 18:00:13 +0800

    add policies;

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,16 +13,26 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-        'App\Models\Address' 		    => 'App\Policies\RootPolicy',
-        'App\Models\Banner' 		    => 'App\Policies\BannerPolicy',
-        'App\Models\Cart' 		        => 'App\Policies\RootPolicy',
-        'App\Models\Category' 		    => 'App\Policies\CategoryPolicy',
-        'App\Models\Goods'             => 'App\Policies\GoodsPolicy',
-        'App\Models\Order' 		       => 'App\Policies\OrderPolicy',
-        'App\Models\Revenue' 		    => 'App\Policies\RevenuePolicy',
-        'App\Models\Store' 		       => 'App\Policies\StorePolicy',
-        'App\Models\User'              => 'App\Policies\UserPolicy',
+        'App\Models\Address' 	=> 'App\Policies\AddressPolicy',
+        'App\Models\Banner' 	=> 'App\Policies\BannerPolicy',
+        'App\Models\Cart'   	=> 'App\Policies\CartPolicy',
+        'App\Models\Category' 	=> 'App\Policies\CategoryPolicy',
+        'App\Models\City'   	=> 'App\Policies\CityPolicy',
+        'App\Models\Clerk'  	=> 'App\Policies\ClerkPolicy',
+        'App\Models\Customer' 	=> 'App\Policies\CustomerPolicy',
+        'App\Models\Device' 	=> 'App\Policies\DevicePolicy',
+        'App\Models\District' 	=> 'App\Policies\DistrictPolicy',
+        'App\Models\Expert' 	=> 'App\Policies\ExpertPolicy',
+        'App\Models\Goods'  	=> 'App\Policies\GoodsPolicy',
+        'App\Models\Logistic' 	=> 'App\Policies\LogisticPolicy',
+        'App\Models\Order'  	=> 'App\Policies\OrderPolicy',
+        'App\Models\Province' 	=> 'App\Policies\ProvincePolicy',
+        'App\Models\Revenue' 	=> 'App\Policies\RevenuePolicy',
+        'App\Models\Salesman' 	=> 'App\Policies\SalesmanPolicy',
+        'App\Models\Setting' 	=> 'App\Policies\SettingPolicy',
+        'App\Models\Store'  	=> 'App\Policies\StorePolicy',
+        'App\Models\Supplier' 	=> 'App\Policies\SupplierPolicy',
+        'App\Models\User'   	=> 'App\Policies\UserPolicy',
     ];
 
     /**
