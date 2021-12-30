323d8baf55809c57e72b68d9d696c4d5ab5aad22
Bourne <68225384@qq.com>
2021-12-30 15:53:25 +0800

    fix user avatar;

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -1,9 +1,8 @@
 <dropdown-trigger class="h-9 flex items-center">
     @isset($user->email)
-        <img
-            src="https://secure.gravatar.com/avatar/{{ md5(\Illuminate\Support\Str::lower($user->email)) }}?size=512"
-            class="rounded-full w-8 h-8 mr-3"
-        />
+        <svg xmlns="http://www.w3.org/2000/svg" class="rounded-full w-8 h-8 mr-3 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
     @endisset
 
     <span class="text-90">
