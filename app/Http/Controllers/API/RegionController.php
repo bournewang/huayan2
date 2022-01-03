f8f6fd6546a53ee521e53f95f021722efe1ae5e1
Bourne <68225384@qq.com>
2022-01-03 20:49:39 +0800

    fix region api docs;

--- app/Http/Controllers/API/RegionController.php
+++ app/Http/Controllers/API/RegionController.php
@@ -7,11 +7,30 @@ use App\Models\City;
 
 class RegionController extends ApiBaseController
 {
+    /**
+     * Provinces list api
+     *
+     * @OA\Get(
+     *  path="/api/provinces",
+     *  tags={"Region"},
+     *  @OA\Response(response=200,description="successful operation")
+     * )
+     */  
     public function provinces(Request $request)
     {
         return (hash2array(Province::all()->pluck('name','id')->all()));
     }
     
+    /**
+     * Cities list api
+     *
+     * @OA\Get(
+     *  path="/api/provinces/{province_id}/cities",
+     *  tags={"Region"},
+     *  @OA\Parameter(name="province_id",in="path",required=true,explode=true,@OA\Schema(type="integer"),description="province_id"),
+     *  @OA\Response(response=200,description="successful operation")
+     * )
+     */  
     public function cities($province_id, Request $request)
     {
         if ($province = Province::find($province_id)) {
@@ -19,6 +38,16 @@ class RegionController extends ApiBaseController
         }
     }
     
+    /**
+     * Districts list api
+     *
+     * @OA\Get(
+     *  path="/api/cities/{city_id}/districts",
+     *  tags={"Region"},
+     *  @OA\Parameter(name="city_id",in="path",required=true,explode=true,@OA\Schema(type="integer"),description="city_id"),
+     *  @OA\Response(response=200,description="successful operation")
+     * )
+     */  
     public function districts($city_id, Request $request)
     {
         if ($city = City::find($city_id)) {
